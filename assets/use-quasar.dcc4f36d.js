ee028fbb887877491c42b9d962e3dbb8856ee585
JosipGS <jspnvk@gmail.com>
2025-12-16 14:34:12 +0100

    test

---
+++ assets/use-quasar.dcc4f36d.js
@@ -0,0 +1,1 @@
+import{c as r}from"./vm.a84afd58.js";import{X as a,D as s,g as i,i as m,a4 as l}from"./index.077cbaab.js";function T(){let e=null;const n=i();function t(){e!==null&&(clearTimeout(e),e=null)}return a(t),s(t),{removeTimeout:t,registerTimeout(u,o){t(),r(n)===!1&&(e=setTimeout(()=>{e=null,u()},o))}}}function v(){return m(l)}export{T as a,v as u};
